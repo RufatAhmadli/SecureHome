69f1d9f91e5bf79a997a2e22ea6aa59322b05263
Rufat <ahmadlirufat10@gmail.com>
2026-02-15 02:14:53 +0400

    First commit

---
+++ src/test/java/example/web/securehome/SecureHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package example.web.securehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
